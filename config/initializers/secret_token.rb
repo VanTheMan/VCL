6571b630c50f3ab1a1b65c1bf4d6fc25ec6e9f31
Toan Nguyen <toannm3110@gmail.com>
2012-08-20 16:40:30 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VCL::Application.config.secret_token = 'e7d66d277cd7c6d88b37d3d0c89ec37e51ce33f8786f3dbaae3cc691b3b6d51ca799482a6832d9e28d3a58cfc4966ba7645eeef799f2e22b580451e1ca2eb077'
